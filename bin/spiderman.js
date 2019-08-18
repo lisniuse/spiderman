0ed05b18fc86c4216665fe814205106518662105
lisniuse <17560235@qq.com>
2019-08-18 14:15:25 +0800

    feat: add async function.

--- bin/spiderman.js
+++ bin/spiderman.js
@@ -1,19 +1,17 @@
 #!/usr/bin/env node
 
-const config = require('../lib/config/load')();
+const config = require('../lib/config/load');
 const Spiderman = require('../lib/class/Spiderman');
 
 (async function main() {
-
+  let conf = await config();
   let spiderman = new Spiderman({
-    config,
+    config: conf,
     urlCallback: function(url) {
       if (config.printUrl === true) {
         console.log('request:', url);
       }
     }
   });
-
   await spiderman.start();
-
 })();
